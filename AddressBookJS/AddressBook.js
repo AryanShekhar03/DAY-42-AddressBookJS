888f17c6fd1b6f535127767575acaa505f12db43
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-23 18:32:18 +0530

    Sorting the address book according to city,state or zip

--- AddressBookJS/AddressBook.js
+++ AddressBookJS/AddressBook.js
@@ -186,4 +186,15 @@ console.log("Number of people from that city is: "+peopleFromCity.length);
 let sortedArray = addressArr.sort(a=>a.firstName);
 console.log(sortedArray);
 
+//UC12 -Sorting the address book according to city,state or zip
+let sortedArray_state= addressArr.sort((a,b)=>a.state.localeCompare(b.state));
+console.log(sortedArray_state);
+
+let sortedArray_city = addressArr.sort((a,b)=>a.city.localeCompare(b.city));
+console.log(sortedArray_city);
+
+let sortedArray_zip = addressArr.sort((a,b)=>(a.zip>b.zip)?1:(b.zip>a.zip)?-1:0);
+console.log(sortedArray_zip);
+
+
 
