26c4bf692471ad6f956716e042fc1a1e080cc106
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-23 17:49:18 +0530

    Ability to find existing contact person using their name

--- AddressBookJS/AddressBook.js
+++ AddressBookJS/AddressBook.js
@@ -120,3 +120,22 @@ catch(e)
     console.error(e);
 }
 
+//UC-4
+
+function SearchName(addressbook)
+{
+    if(addressbook.firstName=="Rahul") return addressbook;
+}
+let searchResult =addressArr.find(SearchName);
+console.log("Result after searching "+searchResult);
+
+//UC5-Delete an element using the firstname
+// function DeleteAddress(addressbook){
+//     let resultAddress = SearchName(addressbook);
+//     var index= addressArr.indexOf(resultAddress);
+//     return index;
+// }
+// let deleteIndex=addressArr.find(DeleteAddress);
+// console.log(addressArr.splice(deleteIndex,3)+" is deleted")
+
+
