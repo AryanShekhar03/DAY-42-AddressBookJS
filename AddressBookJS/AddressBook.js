6d584921f510819562f4035a8d0b4444bdccfdcb
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-23 18:23:24 +0530

    Viewing person by city/state

--- AddressBookJS/AddressBook.js
+++ AddressBookJS/AddressBook.js
@@ -166,5 +166,15 @@ catch(e)
 // }    
 
 //UC8
-let peopleFromCity=addressArr.filter(a=>a.city.includes("Pune"));
-console.log("People from the given city are: "+peopleFromCity);
+// let peopleFromCity=addressArr.filter(a=>a.city.includes("Pune"));
+// console.log("People from the given city are: "+peopleFromCity);
+
+//UC9-Viewing person by city/state
+function ReturnCityMap(a)
+{
+    return a.city+ "->"+a.firstName;
+}
+
+let addressCityMap= addressArr.map(ReturnCityMap);
+console.log("Viewing the people by their city")
+console.log(addressCityMap);
