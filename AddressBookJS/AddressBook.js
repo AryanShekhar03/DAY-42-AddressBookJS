8bac527d8aa27bcf94a144eaf68d7f5334c14fa3
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-23 18:30:08 +0530

    Sorting the address book according to person's name

--- AddressBookJS/AddressBook.js
+++ AddressBookJS/AddressBook.js
@@ -179,6 +179,11 @@ let addressCityMap= addressArr.map(ReturnCityMap);
 console.log("Viewing the people by their city")
 console.log(addressCityMap);
 
-//UC10-Getting count of people living in a city/state
+//UC10
 console.log("Number of people from that city is: "+peopleFromCity.length);
+
+//UC11-Sorting the address book according to person's name
+let sortedArray = addressArr.sort(a=>a.firstName);
+console.log(sortedArray);
+
 
